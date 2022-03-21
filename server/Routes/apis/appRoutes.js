edc1887870b9b94df0b596b63d1c98525380fbf6
callistus406 <callistus406@gmail.com>
2022-03-21 15:29:12 +0100

    added some routes

--- server/Routes/apis/appRoutes.js
+++ server/Routes/apis/appRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 //
 const dotenv = require("dotenv");
 dotenv.config();
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 // require database
 // const db = require("../../dbConnection")
 const mysql = require("mysql");
@@ -18,7 +19,7 @@ const db = mysql.createPool({
 // route for login
 
 router.get("/login", (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   const sql =
     "SELECT * FROM virtual_atm.customers_acct_info WHERE acct_no=3121567789";
 
@@ -29,9 +30,25 @@ router.get("/login", (req, res) => {
 });
 // route for deposit
 router.post("/changePassword", (req, res) => {
-  console.log(req.body);
-});
+  const {oldPassword,newPassword,retypePassword}= req.body;
+  const sql =
+  "UPDATE virtual_atm.customers_acct_info SET balance = ? WHERE acct_no= ?";
 
+
+
+});
+router.post("/get_user_password", (req, res) => {
+  const { account } = req.body;
+  // let ans = ["tytytyt"];
+  console.log(account, "yea");
+  console.log("..........");
+  const sql =
+    "SELECT pin FROM virtual_atm.customers_acct_info WHERE acct_no = ? ";
+  db.query(sql, account, (error, result) => {
+    if (error) console.log(error);
+    res.send(result);
+  });
+});
 // route for bank_details
 router.put("/bank_details", (req, res) => {
   const { account, amount } = req.body;
@@ -71,24 +88,42 @@ router.put("/deposit", (req, res) => {
 });
 router.put("/withdraw", (req, res) => {
   const { amount, account } = req.body;
-  console.log(amount, account);
+  // console.log(amount, account);
   // const sql =""
   const sql =
     "UPDATE virtual_atm.customers_acct_info SET balance = ? WHERE acct_no= ?";
   db.query(sql, [amount, account], (error, result) => {
     if (error) console.log(error);
-    console.log(result);
+    // console.log(result);
+  });
+});
+router.put("/update_bank", (req, res) => {
+  const { amount, account } = req.body;
+  console.log(amount);
+  // const sql =""
+  const sql = "UPDATE virtual_atm.bank SET Balance = ?";
+  db.query(sql, amount, (error, result) => {
+    if (error) console.log(error);
+    // console.log(result);
   });
 });
 // route for transfer
 router.post("/transfer", (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
 });
 
 // route for betting amount
 
-router.post("/betting_amount_details", (req, res) => {
+router.put("/betting_amount", (req, res) => {
+  console.log("meeeeeeeeeeeee");
   console.log(req.body);
+  const { amount, account } = req.body;
+  const sql =
+    "UPDATE virtual_atm.customers_acct_info SET balance = ? WHERE acct_no= ?";
+  db.query(sql, [amount, account], (error, result) => {
+    if (error) console.log(error);
+    // console.log(result);
+  });
 });
 router.get("/bank_balance", (req, res) => {
   const sql = "SELECT * FROM virtual_atm.bank ";
@@ -100,11 +135,19 @@ router.get("/bank_balance", (req, res) => {
 
 router.post("/get_user_balance", (req, res) => {
   const { account } = req.body;
+  // let ans = ["tytytyt"];
   console.log(account, "yea");
   console.log("..........");
   const sql =
     "SELECT balance FROM virtual_atm.customers_acct_info WHERE acct_no = ? ";
   db.query(sql, account, (error, result) => {
+    if (error) console.log(error);
+    res.send(result);
+  });
+});
+router.post("/get_bank_balance", (req, res) => {
+  const sql = "SELECT * FROM virtual_atm.bank";
+  db.query(sql, (error, result) => {
     if (error) console.log(error);
     res.send(result);
   });
